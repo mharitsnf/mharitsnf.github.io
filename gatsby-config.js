33c63496bd8519cc5020fe2c44bb49629088cbc5
Harits Nur Fauzan <mharitsnf@gmail.com>
2023-11-18 10:41:05 +0100

    Initial commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,7 +4,22 @@
 module.exports = {
   siteMetadata: {
     title: `Portfolio`,
+    description: `Harits' Portfolio`,
+    twitterUsername: `@mharitsnf`,
+    image: `/gatsby-icon.png`,
     siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: [],
+  plugins: [
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: `blog`,
+        path: `${__dirname}/blog`,
+      }
+    },
+    "gatsby-plugin-mdx",
+    "gatsby-transformer-sharp",
+  ],
 }

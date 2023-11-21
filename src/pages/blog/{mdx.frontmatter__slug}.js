b64181141e600a48c51346adf65fde7988369cad
Harits Nur Fauzan <mharitsnf@gmail.com>
2023-11-21 13:38:58 +0100

    add gh-pages

--- src/pages/blog/{mdx.frontmatter__slug}.js
+++ src/pages/blog/{mdx.frontmatter__slug}.js
@@ -1,25 +1,12 @@
 import * as React from 'react'
 import { graphql } from 'gatsby'
-import { GatsbyImage, getImage } from 'gatsby-plugin-image'
 import PageContainer from '../../components/layout'
 import MDXLayout from '../../components/mdx_components'
-import { DividerLg } from '../../components/content'
 import Seo from '../../components/seo'
 
 const BlogPost = ({ data, children }) => {
-  const image = getImage(data.mdx.frontmatter.hero_image)
-
   return (
     <PageContainer mode="CONTENT" extData={data}>
-
-      {/* Hero img */}
-      <GatsbyImage
-        className='rounded-lg'
-        image={image}
-        alt={data.mdx.frontmatter.hero_image_alt}
-      />
-
-      <DividerLg />
 
       <MDXLayout>
         {children}
